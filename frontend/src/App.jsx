7bbf2f16a88d874776ecdf9bed28ae9127aaa8ef
Murali Krishna <krishnamurali.barla@sasi.ac.in>
2024-07-22 00:27:39 +0530

    Created Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Home from './components/Home';
+import About from './components/About';
+import Profile from './components/Profile';
 
 export default function App() {
-  return (
-    <div>
-      <h1 className='text-red-500'>React Project</h1>
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+
+    <Route path='/home' element={ <Home/> }/>
+    <Route path='/signup' element={ <Signup/> }/>
+    <Route path='/login' element={ <Login/> }/>
+    <Route path='/about' element={ <About/> }/>
+    <Route path='/profile' element={ <Profile/> }/>
+
+  </Routes>
+
+  </BrowserRouter>
 }
